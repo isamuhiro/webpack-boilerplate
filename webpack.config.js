4540305ba9ea9f7ee226faf617af6bf371b61abf
Paweł Preczyński <pawel.preczynski@gmail.com>
2018-10-23 20:19:12 +0200

    Added babel, updated project name.

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,19 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
